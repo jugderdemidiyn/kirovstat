3658bffd495c64b54933d805426049af16a7236d
Jugderdemidiyn <ieyasu@mail.ru>
2025-04-02 17:43:46 +0300

    save

--- mbstat/kirovstat/views2.py
+++ mbstat/kirovstat/views2.py
@@ -84,19 +84,20 @@ def add_game (request):
         f_name=request.FILES['file']
 
         dl=parse_excel_to_dict_list(f_name)
-        #print (dl)
+        
         tours = 7
         
+        global cheked_list
         cheked_list=[]
         cheked_g_name={}
 
         for i in dl:
-         # print (i)
+         
             j=list(i.values())
             cheked_list_line={}
        
             a_name,a_id = check_team_name (j[0])
-            #print(j[0],' ',a_name,' ',a_id)
+            
             cheked_list_line['dl_name']=j[0]
             cheked_list_line['ch_name']=a_name
             cheked_list_line['ch_id']=a_id
@@ -109,18 +110,35 @@ def add_game (request):
             cheked_list.append(cheked_list_line)
 
             g_name,g_id=check_game_name(list(i.keys())[0])
-            #print (list(i.keys())[0])
-            #print (g_name,' ',g_id)
             cheked_g_name [g_name]= g_id
-        
+            global game_id_for_add
+            game_id_for_add=g_id
 
-        context = { 'dl' : cheked_list , 'gl' : cheked_g_name }
-        print (cheked_g_name)
-        return render (request, 'load_game.html', context)
+        context1 = { 'dl' : cheked_list , 'gl' : cheked_g_name }
+        #print (cheked_g_name)
+        return render (request, 'load_game.html', context1)
     
     if request.method == 'GET' and request.GET.get('add_yes'):
-        print('OOOOOKkk')
         
+        print(cheked_list)
+
+        for i in cheked_list:
+            new_data=gmdata(
+            gd_game=game_id_for_add,
+            gd_team=i['ch_id'],
+            gd_set1=i[1], 
+            gd_set2=i[2],
+            gd_set3=i[3], 
+            gd_set4=i[4], 
+            gd_set5=i[5], 
+            gd_set6=i[6], 
+            gd_set7=i[7], 
+            gd_summ=i['summ'], 
+            gd_place = i['place']
+            )
+            new_data.save()
+
+        return HttpResponse (html)
 
 
     return render(request, 'add_game_file.html')
