fefc3dac179b58ac00ea170d3e305e6a96f49304
Artem <ieyasu@mail.ru>
2025-04-09 15:27:47 +0300

    база для рейтингов

--- mbstat/kirovstat/models.py
+++ mbstat/kirovstat/models.py
@@ -71,4 +71,20 @@ class gmdata (models.Model):
         self.ret1 = self.gd_game.g_name + " - " + str(self.gd_place) + " место "
         return self.ret1
            
-       
+class weekr (models.Model):
+    
+    week_year = models.CharField(default="Номер недели и год", max_length=100)
+    week_start = models.DateField(verbose_name='Начало недели')
+    week_end = models.DateField(verbose_name='Конец недели')
+    week_points_tuz = models.CharField(default="Очки команд за музыкалку",max_length=500)
+    week_points_сlass = models.CharField(default="Очки команд за классику",max_length=500)
+    week_points_summ = models.CharField(default="Очки команд всего",max_length=500)
+    week_rating_tuz = models.CharField(default="10 рейтинга за музыкалку",max_length=200)
+    week_rating_сlass = models.CharField(default="10 рейтинга за классику",max_length=200)
+    week_rating_summ = models.CharField(default="10 рейтинга всего",max_length=200)
+    
+
+    class Meta: 
+       verbose_name_plural = 'Статистика по неделям' 
+       verbose_name = 'Статистика' 
+       ordering = ['week_start'] 
